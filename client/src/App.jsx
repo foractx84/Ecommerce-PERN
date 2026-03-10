74d472d64255f6b276d4e67cb6bb0261571a1778
devity84pro <devity84.pro@gmail.com>
2026-03-09 20:57:20 -0400

    feat: integrate Tailwind CSS and Storybookn Btn component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
-
-import './App.css'
+import Button from './components/ui/Button'
 
 function App() {
   return (
-      <h1>Hello World</h1>  
-    )
+    <div className="p-10 bg-black">
+      <h1 className="text-4xl text-red-500 font-bold">
+        Tailwind works
+      </h1>
+      <Button variant="primary">Primary Button</Button>
+    </div>
+  )
 }
 
-export default App;
+export default App
